a9d446854d36212c5b1efa37d71e9fee6c6bd707
Ilya Pudov <ilya.pudov.dev@gmail.com>
2025-04-02 22:56:24 +0300

    Constaтts rework for statistics

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -6,17 +6,19 @@ from time import sleep, time
 MINIMUM = 1
 MAXIMUM = 100
 # chances
-EASY = 10
-NORMAL = 7
-HARD = 3
+DIFFICULTIES = {'Easy': 10,
+                'Normal': 7,
+                'Hard': 3,
+                'Extra': 0}
 
 
-def validate_yes_or_no(user_answer):
+def validate_yes_or_no(user_answer: object) -> str:
+    '''Ask input until it equal to 'y' or 'n'.'''
     while user_answer != 'y' and user_answer != 'n':
         user_answer = input('I don\'t understand you. PLease enter just "y" or "n" : ')
     return user_answer
 
-def welcome():
+def welcome() -> None:
     flush_print = lambda message: print(message, end='', flush=True)
     print('_' * 61, end='\n\n')
     flush_print('Welcome to the one...')
@@ -34,42 +36,41 @@ def welcome():
         flush_print(':D :D ')
     print('\n')
 
-def set_difficulty():
-    difficulty_levels = {1: EASY,
-                         2: NORMAL,
-                         3: HARD}
+def set_difficulty() -> tuple[str, bool]:
+    '''To choose difficulty level and whether extended hints are on.
+    Depends on DIFFICULTIES constant.'''
     print('Please, set difficulty.', end='\n\n')
     sleep(0.5)
-    print(f'1. Easy ({EASY} chances)')
-    sleep(0.3)
-    print(f'2. Normal ({NORMAL} chances)')
-    sleep(0.3)
-    print(f'3. Hard ({HARD} chances)') 
-    sleep(0.3)
+    for counter, (level, chances) in enumerate(DIFFICULTIES.items(), 1):
+        print(f'{counter}. {level} ({chances} chances)')
+        sleep(0.3)
     print()
-    picked_level = input('Enter the corresponding number [1/2/3]: ')
-    while not picked_level.isnumeric() or not 0 < int(picked_level) < 4:
-        picked_level = input('PLease, enter "1", "2" or "3": ')
+    picked_level = input(f'Enter the corresponding number [{'/'.join(str(num) for num in range(1, counter + 1))}]: ')
+    while not picked_level.isnumeric() or not int(picked_level) in range(1, counter + 1):
+        picked_level = input(f'PLease, enter one of these - {', '.join(str(num) for num in range(1, counter + 1))}: ')
+    picked_level = list(DIFFICULTIES)[int(picked_level) - 1]
     print('...')
     sleep(0.5)
     print('OK, I got it.')
     print()
     hints_state = input('Do you want to turn hints on? [y/n] : ')
-    hints_state = validate_yes_or_no(hints_state)
-    return difficulty_levels[int(picked_level)], True if hints_state == 'y' else False
+    hints_state = True if validate_yes_or_no(hints_state) == 'y' else False
+    return picked_level, hints_state
 
-def take_guess(chances):
+def take_guess(chances:int) -> int:
+    '''Return the guess number'''
     guess = input(f'Guess it ({chances} attempts left): ')
     while not guess.isnumeric():
         guess = input('The answer is integer number. Try again: ')
     return int(guess)
 
-def congratulate():
+def congratulate() -> None:
     print('Congratulations!', end=' ', flush=True)
     sleep(0.5)
     print('You won!!!')
 
-def give_hint(answer, guess, hints_state=True):
+def give_hint(answer: int, guess: int, hints_state: bool = True) -> None:
+    '''Give standard (lower/bigger) or extended hint depending on hints_state.'''
     print(f'The number is {'less' if answer < guess else 'bigger'} then {guess}')
     if hints_state:
         if answer == 1:
@@ -83,19 +84,23 @@ def give_hint(answer, guess, hints_state=True):
             print(f'*Hint: the number is {'not ' if answer % random_number != 0 else ''}'
                 f'devisible by {random_number}')
 
-def lose_announcement(answer):
+def lose_announcement(answer: int) -> None:
     print('Oops...', sep=' ', flush=True)
     sleep(1)
     print('You lost!')
     sleep(0.5)
     print(f'The number was {answer}.')
 
-def play_round(chances, hints_state=False):
+def play_round(difficulty: str, hints_state: bool = False) -> tuple[bool, int, int]:
+    '''One number to guess with the difficulty difficulty level and standard or extended hints. 
+    Returns whether user won or not, chances left (-1 if lost), time elapsed (-1 if lost).
+    Depends on MINIMUM, MAXIMUM and DIFFICULTIES constants.'''
     answer = randint(MINIMUM, MAXIMUM)
     print()
     print(f'I\'m thinking of a number between {MINIMUM} and {MAXIMUM}.')
     sleep(1.5)
     count_start = time()
+    chances = DIFFICULTIES[difficulty]
     while chances > 0:
         print()
         guess = take_guess(chances)
@@ -104,8 +109,9 @@ def play_round(chances, hints_state=False):
         sleep(0.15)
         if answer == guess:
             count_stop = time()
+            elapsed_time = int(count_stop - count_start)
             congratulate()
-            print(f'It took you {int(count_stop - count_start)} seconds to guess.')
+            print(f'It took you {elapsed_time} seconds to guess.')
             victory = True
             break
         chances -= 1
@@ -115,21 +121,26 @@ def play_round(chances, hints_state=False):
         print()
         lose_announcement(answer)
         victory = False
+        elapsed_time = -1
     print()
-    return victory
+    return victory, chances - 1, elapsed_time
+
+def update_statistics(difficulty_level, hints_state, chances_left, time_elapsed):
+    pass # is to be implemented
 
 def main():
     welcome()
     sleep(0.5)
-    chances, hints_state = set_difficulty()
+    difficulty, hints_state = set_difficulty()
     while True:
-        victory = play_round(chances, hints_state)
+        victory, chances_left, time_elapsed = play_round(difficulty, hints_state)
+        if victory:
+            update_statistics(difficulty, hints_state, chances_left, time_elapsed)
         victory_message = 'You are amazing! It was SO fun. Play again?'
         lose_message = 'Cheer up! You almost did it. Try again?'
         message = victory_message if victory else lose_message
         answer = input(message + ' [y/n] : ')
-        while answer != 'y' and answer != 'n':
-            answer = input('I don\'t understand you. PLease enter just "y" or "n" : ')
+        answer = validate_yes_or_no()
         if answer == 'n':
             print()
             print('Well, it was fun!',  flush=True, end=' ')
@@ -139,4 +150,5 @@ def main():
             print('\n')
             break
 if __name__ == '__main__':
-    main()
+    # main()
+    print(play_round('Easy', False))
